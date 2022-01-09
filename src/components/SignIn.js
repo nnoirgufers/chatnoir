66bcec1f2b584cc7d544696b074a50becb8b9ebe
nnoirgufers <97391748+nnoirgufers@users.noreply.github.com>
2022-01-09 16:23:07 +0300

    Create SignIn.js

---
+++ src/components/SignIn.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import firebase from 'firebase'
+import { auth } from '../firebase.js'
+import GoogleButton from 'react-google-button'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function SignIn() {
+    function signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+    }
+    return (
+
+
+        <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
+            
+            
+            <GoogleButton
+             label='Google ile giriş yap'
+            onClick={signInWithGoogle}/>
+    
+        </div>
+    )
+}
+
+export default SignIn
+
