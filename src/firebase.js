fb07033a83278c5a76c645588d10f042e44c343c
nnoirgufers <97391748+nnoirgufers@users.noreply.github.com>
2022-01-09 17:18:44 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC_730ZmO8rOnJv02KEBnaydc5d9KlnwLA",
-    authDomain: "firechat-eb0f5.firebaseapp.com",
-    projectId: "firechat-eb0f5",
-    storageBucket: "firechat-eb0f5.appspot.com",
-    messagingSenderId: "855670603066",
-    appId: "1:855670603066:web:a9bcf2477bcbd1045da403",
-    measurementId: "G-GEJDXM80CV"
+    apiKey: "AIzaSyBfOLhoXzfAWNN01w12cWzucCkslNWxi34",
+    authDomain: "noirgufers-56210.firebaseapp.com",
+    projectId: "noirgufers-56210",
+    storageBucket: "noirgufers-56210.appspot.com",
+    messagingSenderId: "1072288931695",
+    appId: "1:1072288931695:web:ecd47f4ff17e9b7c5b929a",
+    measurementId: "G-6EPW107KHV"
 })
 
 const db = firebaseApp.firestore()
