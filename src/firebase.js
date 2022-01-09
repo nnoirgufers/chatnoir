bd2dd057587b54308aa39f4b3af725797f23c5ed
nnoirgufers <97391748+nnoirgufers@users.noreply.github.com>
2022-01-09 17:00:36 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAADI4Uku0sUJU6NC-GcOCqh4b34iPUncc",
     authDomain: "noir-chat.herokuapp.com",
     projectId: "noirchat-ea0b0",
-    storageBucket: "noirchat-ea0b0.appspot.com",
+    storageBucket: "noir-chat.herokuapp.com",
     messagingSenderId: "830454769514",
     appId: "1:830454769514:web:a3a8e6825e0b587ee7a713",
     measurementId: "G-J18GZ4QT92"
